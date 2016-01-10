253e9ad89ab853933f5effa289d36c0d71e6a7d4
yaelschlotman <koshercouponmom@gmail.com>
2016-01-10 00:40:17 -0600

    consolidate elimSpace and elimPunct to one line

--- pal.js
+++ pal.js
@@ -32,7 +32,7 @@ function showElimSpace() {
 
 //showElimSpace();
 
-function showElimPunt() {
+function showElimPunct() {
 	var term = getUserInput("Enter Phrase.");
     var lowercase = term.toLowerCase();
 	var elimSpace = lowercase.replace(/\s/g, "");
@@ -44,9 +44,18 @@ function showElimPunt() {
 
 }
 
-showElimPunt();
+//showElimPunct();
 
+function showElimSpacePunct() {
+	var term = getUserInput("Enter Phrase.");
+    var lowercase = term.toLowerCase();
+	var elimSpacePunct = lowercase.replace(/\s/g && /\W/g, "");
+	console.log(term);
+	console.log(lowercase);
+	console.log(elimSpacePunct);
+}
 
+showElimSpacePunct();
 
 //function palindromeCheck() {
 //  reverse term, compare equality.
